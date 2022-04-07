33a7a13f0e60e7bbac6a268b4f3348dae96d394e
Thiago M Moura <17tawi@gmail.com>
2022-04-06 21:12:17 -0700

    linkedListFind

---
+++ linked_list/linkedListFind.js
@@ -0,0 +1,29 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+const linkedListFind = (head, target) => {
+  // todo
+    let current = head 
+    while(current){
+        if (current.val === target) return true 
+        current = current.next 
+    }
+    return false 
+};
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+// a -> b -> c -> d
+
+console.log(linkedListFind(a, "c")); // true
