ee5ea50e024498995f04e20b97e6a5c306c63ea3
Thiago M Moura <17tawi@gmail.com>
2022-04-10 01:14:01 -0700

    treeMinValue

--- binary_tree/treeIncludes.js
+++ binary_tree/treeIncludes.js
@@ -6,20 +6,25 @@ class Node {
   }
 }
 
+// const treeIncludes = (root, target) => {
+//   if (root === null) return false;
+//   if (root.val === target) return true;
+//     let queue = [ root ];
+
+//     while (queue.length > 0){
+//         let current = queue.shift();
+//         if (current.val === target) return true 
+
+//         if (current.left) queue.push(current.left);
+//         if (current.right) queue.push(current.right);
+//     }
+//     return false; 
+// };
 const treeIncludes = (root, target) => {
-  if (root === null) return false;
-  if (root.val === target) return true;
-    let queue = [ root ];
-
-    while (queue.length > 0){
-        let current = queue.shift();
-        if (current.val === target) return true 
-
-        if (current.left) queue.push(current.left);
-        if (current.right) queue.push(current.right);
-    }
-    return false; 
-};
+    if (root === null) return false;
+    if (root.val === target) return true;
+    return treeIncludes(root.left, target) || treeIncludes(root.right, target);
+}
 
 const a = new Node("a");
 const b = new Node("b");
---
+++ binary_tree/treeMinValue.js
@@ -0,0 +1,39 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const treeMinValue = (root) => {
+    if (root === null) return Infinity;
+    let min = root.val;
+    let leftNode = treeMinValue(root.left);
+    let rightNode = treeMinValue(root.right);
+
+    if (leftNode < min) min = leftNode;
+    if (rightNode < min) min = rightNode;
+    return min;
+};
+
+const a = new Node(3);
+const b = new Node(11);
+const c = new Node(4);
+const d = new Node(4);
+const e = new Node(-2);
+const f = new Node(1);
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//       3
+//    /    \
+//   11     4
+//  / \      \
+// 4   -2     1
+
+console.log(treeMinValue(a)); // -> -2
