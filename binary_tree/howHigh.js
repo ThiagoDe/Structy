8317fa7de933bc8eaf2ae912bf9ee6c6b6f8c9dd
Thiago M Moura <17tawi@gmail.com>
2022-04-10 12:12:54 -0700

    how Hight tree

---
+++ binary_tree/howHigh.js
@@ -0,0 +1,28 @@
+const { Node } = require('./nodeClass');
+
+const a = new Node('a');
+const b = new Node('b');
+const c = new Node('c');
+const d = new Node('d');
+const e = new Node('e');
+const f = new Node('f');
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+//      a
+//    /   \
+//   b     c
+//  / \     \
+// d   e     f
+
+const howHigh = (node) => {
+    if (node === null) return -1;
+    return 1 + Math.max(howHigh(node.left), howHigh(node.right)) // always add 1
+   
+}
+
+console.log(howHigh(a)); // -> 2
