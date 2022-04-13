9cb7822fac980d12ccce5ecd828084de80380c78
Thiago M Moura <17tawi@gmail.com>
2022-04-12 17:32:17 -0700

    semesters Required

---
+++ graph/semestersRequired.js
@@ -0,0 +1,13 @@
+const numCourses = 6;
+const prereqs = [
+  [1, 2],
+  [2, 4],
+  [3, 5],
+  [0, 5],
+];
+const semestersRequired = (numCourses, prereqs) => {
+    
+};
+
+
+console.log(semestersRequired(numCourses, prereqs)); // -> 3
