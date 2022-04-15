3b76d712a78b5c042bf998966d682355cd8713a8
Thiago <17tawi@gmail.com>
2022-04-14 23:16:41 -0700

    Max Path Sum

--- dynamic_programming/maxPathSum.js
+++ dynamic_programming/maxPathSum.js
@@ -7,19 +7,18 @@ const grid = [
 const maxPathSum = (grid, r= 0, c=0, memo = {}) => {
     const pos = r + ',' + c;
     if (pos in memo) return memo[pos];
-    if (r === grid.length || c === grid[0].length ) return 0;
-    let maxSum = 0;
+
+    if (r === grid.length || c === grid[0].length ) return - Infinity;
     if ( r === grid.length - 1 && c === grid[0].length - 1) {
         memo[pos] = grid[r][c];
-        maxSum = Math.max(memo[pos], maxSum);
+        return memo[pos];
     }
 
-    const sumRight = maxPathSum(grid, r + 1, c, memo);
-    const sumDown = maxPathSum(grid, r, c + 1, memo);
-    memo[pos] = sumRight + sumDown;
+    const sumDown = maxPathSum(grid, r + 1, c, memo);
+    const sumRight = maxPathSum(grid, r, c + 1, memo);
 
-    maxSum = Math.max(memo[pos], maxSum)
-    return maxSum;
+    memo[pos] = grid[r][c] + Math.max(sumRight, sumDown) 
+    return memo[pos];
 };
 
 console.log(maxPathSum(grid)); // -> 18
