6d762e2d8ddf8f81d6c0ee9671d08e874ab2e5c5
Thiago M Moura <17tawi@gmail.com>
2022-04-14 20:59:51 -0700

    count Paths

---
+++ dynamic_programming/countPaths.js
@@ -0,0 +1,13 @@
+const grid = [
+  ["O", "O", "X", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "X"],
+  ["X", "O", "O", "O", "O", "O"],
+  ["X", "X", "X", "O", "O", "O"],
+  ["O", "O", "O", "O", "O", "O"],
+];
+
+const countPaths = (grid) => {
+  // todo
+};
+
+console.log(countPaths(grid)); // -> 42
