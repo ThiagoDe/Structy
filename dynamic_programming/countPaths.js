8ea576f3a1a2ed7f4d06eae7a6f243c67ba11368
Thiago <17tawi@gmail.com>
2022-04-14 22:16:04 -0700

    count Paths

--- dynamic_programming/countPaths.js
+++ dynamic_programming/countPaths.js
@@ -6,8 +6,19 @@ const grid = [
   ["O", "O", "O", "O", "O", "O"],
 ];
 
-const countPaths = (grid) => {
-  // todo
+const countPaths = (grid, r=0, c=0, memo = {}) => {
+  const pos = r + ',' + c;
+  if (pos in memo) return memo[pos];
+  if (r === grid.length || c === grid[0].length || grid[r][c] === 'X') return 0;
+
+  if (r === grid.length - 1 && c === grid[0].length - 1) return 1;
+
+  const downCount = countPaths(grid, r + 1, c);
+  const rightCount = countPaths(grid, r, c + 1);
+
+  memo[pos] = downCount + rightCount;
+  return memo[pos];
+
 };
 
 console.log(countPaths(grid)); // -> 42
