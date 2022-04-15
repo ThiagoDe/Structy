0f3f7687f2a952bf92ff9481eb7760f0590e1b07
Thiago <17tawi@gmail.com>
2022-04-14 23:48:38 -0700

    Non adjacent Sum

--- dynamic_programming/nonAdjacentSum.js
+++ dynamic_programming/nonAdjacentSum.js
@@ -1,7 +1,14 @@
 const nums = [2, 4, 5, 12, 7];
 
-const nonAdjacentSum = (nums) => {
-  // todo
-};
+const nonAdjacentSum = (nums, i = 0, memo = {}) => {
+  if (i in memo) return memo[i];
+  if (i >= nums.length) return 0;
+  
+  const include = nums[i] + nonAdjacentSum(nums, i + 2, memo);
+  const exclude = nonAdjacentSum(nums, i + 1, memo);
+  
+  memo[i] = Math.max(include, exclude);
+  return memo[i];
+}
 
 console.log(nonAdjacentSum(nums)); // -> 16
