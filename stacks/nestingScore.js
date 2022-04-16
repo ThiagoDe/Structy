210210ae031ad926214eefbc9f8f9e134072f195
Thiago <17tawi@gmail.com>
2022-04-16 11:48:17 -0700

    Nesting Score

--- stacks/nestingScore.js
+++ stacks/nestingScore.js
@@ -1,5 +1,20 @@
 const nestingScore = (str) => {
+    const stack = [0];
+    
+    for (let char of str){
+        if (char === '['){
+            stack.push(0);
+        } else {
+            const popped = stack.pop();
+            if (popped === 0) {
+                stack[stack.length - 1] += 1;
+            } else {
+                stack[stack.length - 1] += popped * 2;
+            }
+        }
+    }
 
+    return stack.pop();
 };
 
 console.log(nestingScore("[[][][]]")); // -> 6
