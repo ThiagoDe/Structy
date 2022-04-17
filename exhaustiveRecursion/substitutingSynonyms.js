4c5fc49cada72a91b05a09b932a5d0aa600840d2
Thiago <17tawi@gmail.com>
2022-04-16 19:03:34 -0700

    Substituting Synonyms

---
+++ exhaustiveRecursion/substitutingSynonyms.js
@@ -0,0 +1,20 @@
+const sentence = "follow the yellow brick road";
+const synonyms = {
+  follow: ["chase", "pursue"],
+  yellow: ["gold", "amber", "lemon"],
+};
+
+
+const substituteSynonyms = (sentence, synonyms) => {
+
+};
+
+substituteSynonyms(sentence, synonyms);
+// [
+//   'chase the gold brick road',
+//   'chase the amber brick road',
+//   'chase the lemon brick road',
+//   'pursue the gold brick road',
+//   'pursue the amber brick road',
+//   'pursue the lemon brick road'
+// ]
