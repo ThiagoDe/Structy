621697e2d291eb3cfac29d91784959f9fad23aef
Thiago <17tawi@gmail.com>
2022-04-17 12:42:25 -0700

    Substituting Synonyms

--- exhaustiveRecursion/substitutingSynonyms.js
+++ exhaustiveRecursion/substitutingSynonyms.js
@@ -6,10 +6,45 @@ const synonyms = {
 
 
 const substituteSynonyms = (sentence, synonyms) => {
+    const words = sentence.split(' ');
+    const subarrays = generate(words, synonyms);//
+    // const result = [];
+    return subarrays.map((subarray) => subarray.join(' '));
+    // for (let subarray of subarrays) {
+    //     result.push(subarray.join(' '));
+    // }
 
+    // return result;
 };
 
-substituteSynonyms(sentence, synonyms);
+const generate = (words, synonyms) => {
+    if (words.length === 0) return [[]];
+    const firstWord = words[0];
+    const remaining = words.slice(1);
+
+    const subarrays = generate(remaining, synonyms);
+
+    if (firstWord in synonyms) {
+        const result = [];
+        for (let synonym of synonyms[firstWord]) {
+            for (let subarray of subarrays) {
+            result.push([synonym, ...subarray]);
+            }
+        }
+
+        return result;
+    } else {
+        const result = [];
+
+        for (let subarray of subarrays) {
+            result.push([firstWord, ...subarray]);
+        }
+
+        return result;
+    }
+}
+
+console.log(substituteSynonyms(sentence, synonyms));
 // [
 //   'chase the gold brick road',
 //   'chase the amber brick road',
