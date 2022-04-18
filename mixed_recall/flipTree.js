255ade1c3899e482da63e1dfdf343bd636a9a128
Thiago <17tawi@gmail.com>
2022-04-17 18:53:39 -0700

    Flip Tree

---
+++ mixed_recall/flipTree.js
@@ -0,0 +1,54 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+const flipTree = (root) => {
+    if (root === null) return null;
+
+    const left = flipTree(root.left);
+    const right = flipTree(root.right);
+    root.left = right;
+    root.right = left;
+    
+
+    return root;
+};
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+const g = new Node("g");
+const h = new Node("h");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+e.left = g;
+e.right = h;
+
+//      a
+//    /    \
+//   b      c
+//  / \      \
+// d   e      f
+//    / \
+//    g  h
+
+console.log(flipTree(a)); 
+
+//       a
+//    /    \
+//   c      b
+//  /     /   \
+// f     e    d
+//     /  \
+//    h    g
