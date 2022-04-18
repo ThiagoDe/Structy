f6a923a86325fec60b90a0388e52518296d8c634
Thiago <17tawi@gmail.com>
2022-04-18 15:22:19 -0700

    Max Increasing Subseq

--- mixed_recall/maxIncreasingSubseq.js
+++ mixed_recall/maxIncreasingSubseq.js
@@ -1,7 +1,21 @@
 const numbers = [4, 18, 20, 10, 12, 15, 19];
 
-const maxIncreasingSubseq = (numbers) => {
+const maxIncreasingSubseq = (numbers, i = 0, previous = -Infinity, memo={}) => {
+    const key = i + ',' + previous;
+    if (key in memo) return memo[key];
+    if (i === numbers.length) return 0;
 
+    const current = numbers[i];
+    const options = [];
+    const dontTakeCurrent = maxIncreasingSubseq(numbers, i + 1, previous);
+    options.push(dontTakeCurrent);
+
+    if (current > previous) {
+        const TakeCurrent = 1 + maxIncreasingSubseq(numbers, i + 1, current);
+        options.push(TakeCurrent);
+    }
+    memo[key] = Math.max(...options)
+    return memo[key];
 };
 
 console.log(maxIncreasingSubseq(numbers)); // -> 5
