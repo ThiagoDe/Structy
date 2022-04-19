5f75badac1219a60ba77b98128046cb70b0c91b0
Thiago <17tawi@gmail.com>
2022-04-19 00:15:01 -0700

    Combine Intervals

--- mixed_recall/combineIntervals.js
+++ mixed_recall/combineIntervals.js
@@ -1,12 +1,16 @@
 const combineIntervals = (intervals) => {
     const sortedIntervals = intervals.sort((intervalA, intervalB) => intervalA[0] - intervalB[0])
-    const combined = [ sortedIntervals[0]];
+    const combined = [ sortedIntervals[0]]; // [2, 9]
 
-    for (let currentInterval of sortedIntervals.slice(1)){
+    for (let currentInterval of sortedIntervals.slice(1)){ //[6, 8] , [10, 12], [20, 24]
         const [ lastStart, lastEnd ] = combined[combined.length - 1];
         const [ currentStart, currentEnd ] = currentInterval;
         if ( currentStart <= lastEnd){
-            
+            if ( currentEnd >= lastEnd){
+                combined[combined.length - 1][1] = currentEnd;
+            }
+        } else {
+            combined.push(currentInterval)
         }
     }
 
