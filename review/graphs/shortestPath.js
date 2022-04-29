1105fef71fe2aff22925baa51c7566fbc61046cd
ThiagoDe <17tawi@gmail.com>
2022-04-29 12:17:54 -0700

    Graph Builder

--- review/graphs/shortestPath.js
+++ review/graphs/shortestPath.js
@@ -1,6 +1,21 @@
 const shortestPath = (edges, nodeA, nodeB) => {
   const graph = graphBuilder(edges);
+  const visited = new Set([nodeA]);
+  let queue = [[nodeA, 0]];
   
+  while (queue.length > 0){
+    let [node, level] = queue.shift();
+    if (node === nodeB) return level;
+    
+    for (let neighbor of graph[node]){
+      if (!visited.has(neighbor)){
+        visited.add(neighbor);
+        queue.push([neighbor, level + 1]);
+      }
+      
+    }
+  }
+  return -1;
 };
 
 
@@ -18,3 +33,30 @@ const graphBuilder = (edges) => {
   }
   return graph;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+  shortestPath
+};
