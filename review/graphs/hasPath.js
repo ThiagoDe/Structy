548848a8a3fb19111405caadb43a89c0b44e77a2
ThiagoDe <17tawi@gmail.com>
2022-04-28 17:09:59 -0700

    Has Path

---
+++ review/graphs/hasPath.js
@@ -0,0 +1,9 @@
+const hasPath = (graph, src, dst) => {
+  if (src === dst) return true;
+  
+  for (let neighbor of graph[src]){ 
+    if (hasPath(graph, neighbor, dst)) return true;
+  }
+  return false;
+};
+
