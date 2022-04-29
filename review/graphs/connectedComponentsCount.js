e783d2d7674618aed5c08e796757421d02df4772
ThiagoDe <17tawi@gmail.com>
2022-04-28 17:45:23 -0700

    Connected Components Count

---
+++ review/graphs/connectedComponentsCount.js
@@ -0,0 +1,18 @@
+const connectedComponentsCount = (graph) => {
+  const visited = new Set();
+  let count = 0;
+  
+  for (let node in graph){
+   count += traverse(graph, node, visited);
+  }
+  return count;
+};
+
+const traverse = (graph, node, visited) => {
+  if (visited.has(String(node))) return 0;
+  visited.add(String(node));
+  for (let neighbor of graph[node]){
+    traverse(graph, neighbor, visited);
+  }
+  return 1;
+}
